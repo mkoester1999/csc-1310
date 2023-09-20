9bb13cf667d293722bc7bf146523fb682c85765f
dumadsss <bkoester42@tntech.edu>
2023-09-20 13:28:01 -0500

    driver & text class
    started driver, and updated Text,getLength() from uint to int.

--- Project 1/driver.cpp
+++ Project 1/driver.cpp
@@ -1,28 +1,57 @@
+/*
+	Header for Text Class
+	Project 1 - CSC1310
+	Mitchell Koester & Bryant Koester
+	9/25/23
+*/
+
 #include <fstream>
 #include<iostream>
 #include "VideoGame.h"
+#include "VideoGameLibrary.h"
 
 using namespace std;
 
 int main()
-{
+{   //program asks the user “How many video games can your library hold?” Once it reads in this number, it 
+    //should dynamically allocate a VideoGameLibrary object, sending this number as an argument.
+    int maxGamesSize; //max number of games the library can hold
+    int choice; //user input for menu
+    bool endProgram = false; //exit condition for do while loop
+    cout<<"How many video games can your library hold?"<<endl;
+    cin>>maxGamesSize;
+    VideoGameLibrary* videogameLibrary = new VideoGameLibrary(maxGamesSize);
+    do{
+    //create a 6 case switch statement that will allow the user to choose from the following options:
+    //1:Load game from file
+    //2:Save video game to file
+    //3:add video game to library
+    //4:remove a video game from library
+    //5:display all video games
+    //6:remove all video games from library and end program using do while set to a boolean variable
 
-    //declare videogame object with parameters for Text class
-    VideoGame* videogame = new VideoGame(new Text("The Legend of Zelda"), new Text("Nintendo"), new Text("Nintendo"), 1986);
-    videogame->PrintVideoGameDetails();
-    fstream file;
-
-    file.open("videogame.txt", ios::out);
-    videogame->PrintVideoGameDetailsToFile(file);
-    file.close();
-
-    Text* text;
-
-    text = videogame->getVideoGameTitle();
-    text->displayText();
-
-    //delete text;
-    delete videogame;
-    delete text;
-    
+    switch(choice)
+    {
+                    case 1:
+                        //Load game from file
+                        break;
+                    case 2:
+                        //Save video game to file
+                        break;
+                    case 3:
+                        //add video game to library
+                        break;
+                    case 4:
+                        //remove a video game from library
+                        break;
+                    case 5:
+                        //display all video games
+                        break;
+                    case 6:
+                        //remove all video games from library and end program using do while set to a boolean variable
+                        endProgram = true;//exit condition for do while loop
+                        break;
+    }    
+    }while (endProgram == false);
+    return 0;
 }
