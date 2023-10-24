b6993d7abf674d593cee3ffbc139d56d1dcd30bd
mitchell <mkoester1999@hotail.com>
2023-10-24 13:55:54 -0500

    addEntry and validateLogin functions Program 3

--- Projects/Project 3/hashTable.cpp
+++ Projects/Project 3/hashTable.cpp
@@ -44,12 +44,66 @@ string hashTable::getSalt(string username)
 
 }
 
+void hashTable::addEntry(string username, string salt, string passHash)
+{
+	entry* user = new entry(username, salt, passHash);
+
+	//generate hash for the index 
+	int hashIndex = hash(username);
+
+	//check if there is a value at hashIndex already
+	if(!hashArray[hashIndex])
+	{
+		//add user to hashIndex if there is no collisoin
+		hashArray[hashIndex] = user;
+	}
+	else
+	{
+		//make temp entry for traversal
+		entry* temp = hashArray[hashIndex];
+		//iterate through list until next entry is null
+		while(temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = user;
+	}
+	
+
+}
+
+bool hashTable::validateLogin(string username, string hashedPass)
+{
+	//hash username
+	int hashIndex = hash(username);
+	//check if the username at hashed index is equal to the username & password passed
+	if (hashArray[hashIndex] && hashArray[hashIndex]->getUsername() == username && hashArray[hashIndex]->getHashedpwd() == hashedPass)
+	{
+		return true;
+	}
+	//else check if one of the next values at the hashIndex equals username & password
+	else if (hashArray[hashIndex] && hashArray[hashIndex]->next)
+	{
+		//make temp entry for traversal
+		entry * temp = hashArray[hashIndex];
+		//iterate through list until entry is null or temp->username = username and hashed password equals hashed pass
+		while(temp)
+		{
+			if(temp->getUsername() == username && temp->getHashedpwd() == hashedPass) return true;
+			temp = temp->next;
+		}
+		
+	}
+	return false;
+}
+
 //entry class functions-------------------------------------------------
 hashTable::entry::entry(string _uname, string _salt, string _passedHash)
 {
 	uname = _uname;
 	salt = _salt;
 	passHash = _passedHash;
+	next = nullptr;
 
 }
 
